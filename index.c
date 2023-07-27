9f234faddf8ec389112b91c2eccf4a46c50c9032
JosuePimentel <josue.farias.pimentel@gmail.com>
2023-07-27 00:48:55 -0300

    new code

--- index.c
+++ index.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// chave guia (abc) & chave senha (abcde) 
+// chave guia (abc)
 // mensagem
 
-void removeWordWrap( char* mens );
-void criptografarSenha( char mens[50], char* mensCript, char guia[4] );
+void removeWordWrap( char* word );
+void criptografarMensagem( char* mens, char guia[4] );
 
 int main( ) {
 
@@ -22,8 +22,10 @@ int main( ) {
         system("clear");
     #endif
 
+    char mensagem[50];
+    char chaveGuia[4];
     int chase;
-    printf("What do you wanna do?\n [01] Cript\n [02] Uncript\n");
+    printf("What do you wanna do? \n[01] Cript \n[02] Uncript\n");
     scanf("%i", &chase);
 
     #if defined(_WIN32) || defined(_WIN64)
@@ -32,14 +34,11 @@ int main( ) {
         system("clear");
     #endif
 
-    char mensagem[50];
-    char chaveGuia[4];
-    if( chase == 1 ) {
-        printf("Escreva a mensagem que vc quer criptgrafar:\n");
+    if(chase == 1) {
+        printf("Escreva a sua mensagem para ser criptografado: (Sem acentuação) \n");
         getchar();
         fgets(mensagem, sizeof(mensagem), stdin);
-        removeWordWrap( mensagem );
-        char mensagemCriptografada[strlen(mensagem)];
+        removeWordWrap(mensagem);
 
         #if defined(_WIN32) || defined(_WIN64)
             system("cls");
@@ -47,36 +46,31 @@ int main( ) {
             system("clear");
         #endif
 
-        printf("Defina uma chave guia nas seguintes condições: \n01.Contenha 3 caracteres. \n02.Todos eles sem sinais ou acentuação. \n03.Todos minusculos. \n04.De a até z \n");
-        
-        while( 1 ) {
-            fgets(chaveGuia, 4, stdin);
-            removeWordWrap(chaveGuia);
-            if(strlen(chaveGuia) >= 3) break;
-            else printf("Chave guia fora dos padrões\n");
-        }
+        printf("Defina uma palavra guia agora, nas seguintes condições: \n01.Toda minuscula \n02.De a até z. \n03.Contenha no máximo 3 caracteres. \n04.Sem sinais ou acentução\n");
+        fgets(chaveGuia, sizeof(chaveGuia), stdin);
+        // removeWordWrap(chaveGuia);
+        // printf("Strlen: %zu Sizeof: %zu S: %s\n", strlen(chaveGuia), sizeof(chaveGuia), chaveGuia);
 
-        criptografarSenha( mensagem, mensagemCriptografada, chaveGuia );
-        char arquivoEnvioOne[50];
-        char arquivoEnvioTwo[50]; 
-        printf("%zu", strlen(mensagemCriptografada));
-        strcat(arquivoEnvioOne, "\n\nSenha criptografada: ");
-        strcat(arquivoEnvioOne, mensagemCriptografada);
+        criptografarMensagem(mensagem, chaveGuia);
+        // printf("S: %s Size: %zu Len: %zu\n", mensagem, sizeof(mensagem), strlen(mensagem));
+        char mensagemCriptografada[strlen(mensagem)];
+        strcpy(mensagemCriptografada, mensagem);
+        // printf("Size: %zu Len: %zu SMC: %s SM: %s\n", sizeof(mensagemCriptografada), strlen(mensagemCriptografada), mensagemCriptografada, mensagem);
 
-        strcat(arquivoEnvioTwo, "\nchave-guia: ");
-        strcat(arquivoEnvioTwo, chaveGuia);
-        strcat(arquivoEnvioTwo, "\n");
+        char arquivoEnvio[100];
+        strcat(arquivoEnvio, "Mensagem Criptografada: ");
+        strcat(arquivoEnvio, mensagemCriptografada);
+        strcat(arquivoEnvio, "\nChave-guia: ");
+        strcat(arquivoEnvio, chaveGuia);
+        strcat(arquivoEnvio, "\n\n\n");
 
-        size_t tamArqOne = strlen(arquivoEnvioOne);
-        size_t envioOne = fwrite(arquivoEnvioOne, 1, tamArqOne-1, arquivo);
+        size_t TamArq = strlen(arquivoEnvio);
+        size_t enviado = fwrite(arquivoEnvio, 1, TamArq, arquivo);
 
-        size_t tamArqTwo = strlen(arquivoEnvioTwo);
-        size_t envioTwo = fwrite(arquivoEnvioTwo, 1, tamArqTwo, arquivo);
-
-        if(envioOne != tamArqOne && envioTwo != tamArqTwo) {
-            printf("Erro no envio da mensagem!\n");
+        if(enviado != TamArq) {
+            printf("Erro no envio da mensagem ao arquivo!\n");
         } else {
-            printf("Mensagem enviada com sucesso\n");
+            printf("Mensagem enviada ao arquivo com sucesso!\n");
         }
     }
 
@@ -85,26 +79,29 @@ int main( ) {
     return 0;
 }
 
-void removeWordWrap( char* mens ) {
-    mens[strcspn(mens, "\n")] = '\0';
+void removeWordWrap( char* word ) {
+    word[strcspn(word, "\n")] = '\0';
 }
 
-void criptografarSenha( char mens[50], char* mensCript, char guia[4] ) {
+void criptografarMensagem( char* mens, char guia[4] ) {
     int x;
-    size_t tamMsg = strlen(mens);
-    size_t tamGuia = strlen(guia);
-    unsigned int interval[tamGuia];
-
-    for( x = 0 ; x < tamGuia ; x++ ) {
+    size_t tamGUIA = strlen(guia);
+    unsigned int interval[tamGUIA];
+    for( x = 0 ; x < tamGUIA; x++ ) {
         interval[x] = guia[x] - 96;
     }
 
+    size_t tamMSG = strlen(mens);
     int aux = 0;
-    for( x = 0 ; x < tamMsg ; x++ ) {
-        unsigned int word = mens[x];
-        char newWord = word + interval[aux];
+    for( x = 0 ; x < tamMSG ; x++ ) {
+        const unsigned word = mens[x];
+        unsigned int newWord = newWord = word + interval[aux];;
+        if(word == 32) newWord = 35;
+
         if(newWord > 122) newWord -= 57;
-        mensCript[x] = newWord;
+
+        if(newWord > 90 && newWord < 97) newWord += (97 - newWord);
+        mens[x] = newWord;
         aux++;
         if(aux > 2) aux = 0;
     }
