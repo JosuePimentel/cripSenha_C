456b36496c5bff44a9d38a5a2b46c0449d40107c
JosuePimentel <josue.farias.pimentel@gmail.com>
2023-07-27 14:04:20 -0300

    funcion of uncript add/Fix bugs

--- index.c
+++ index.c
@@ -6,6 +6,7 @@
 
 void removeWordWrap( char* word );
 void criptografarMensagem( char* mens, char guia[4] );
+void DescriptografarMensagem( char* mens, char guia[4] );
 
 int main( ) {
 
@@ -22,8 +23,9 @@ int main( ) {
         system("clear");
     #endif
 
-    char mensagem[50];
+    char mensagem[150];
     char chaveGuia[4];
+    char arquivoEnvio[200];
     int chase;
     printf("What do you wanna do? \n[01] Cript \n[02] Uncript\n");
     scanf("%i", &chase);
@@ -48,16 +50,13 @@ int main( ) {
 
         printf("Defina uma palavra guia agora, nas seguintes condições: \n01.Toda minuscula \n02.De a até z. \n03.Contenha no máximo 3 caracteres. \n04.Sem sinais ou acentução\n");
         fgets(chaveGuia, sizeof(chaveGuia), stdin);
-        // removeWordWrap(chaveGuia);
-        // printf("Strlen: %zu Sizeof: %zu S: %s\n", strlen(chaveGuia), sizeof(chaveGuia), chaveGuia);
 
         criptografarMensagem(mensagem, chaveGuia);
-        // printf("S: %s Size: %zu Len: %zu\n", mensagem, sizeof(mensagem), strlen(mensagem));
+
         char mensagemCriptografada[strlen(mensagem)];
         strcpy(mensagemCriptografada, mensagem);
-        // printf("Size: %zu Len: %zu SMC: %s SM: %s\n", sizeof(mensagemCriptografada), strlen(mensagemCriptografada), mensagemCriptografada, mensagem);
 
-        char arquivoEnvio[100];
+
         strcat(arquivoEnvio, "Mensagem Criptografada: ");
         strcat(arquivoEnvio, mensagemCriptografada);
         strcat(arquivoEnvio, "\nChave-guia: ");
@@ -66,6 +65,53 @@ int main( ) {
 
         size_t TamArq = strlen(arquivoEnvio);
         size_t enviado = fwrite(arquivoEnvio, 1, TamArq, arquivo);
+
+        #if defined(_WIN32) || defined(_WIN64)
+            system("cls");
+        #else 
+            system("clear");
+        #endif
+
+        if(enviado != TamArq) {
+            printf("Erro no envio da mensagem ao arquivo!\n");
+        } else {
+            printf("Mensagem enviada ao arquivo com sucesso!\n");
+        }
+    } else if(chase == 2) {
+        printf("Escreva a mensagem criptografada:\n");
+        getchar();
+        fgets(mensagem, sizeof(mensagem), stdin);
+        removeWordWrap(mensagem);
+
+
+        #if defined(_WIN32) || defined(_WIN64)
+            system("cls");
+        #else 
+            system("clear");
+        #endif
+
+        printf("Escreva a palavra guia: \n");
+        fgets(chaveGuia, sizeof(chaveGuia), stdin);
+
+        DescriptografarMensagem(mensagem, chaveGuia);
+
+        char mensagemDescriptografada[strlen(mensagem)];
+        strcpy(mensagemDescriptografada, mensagem);
+
+        strcat(arquivoEnvio, "Mensagem Criptografada: ");
+        strcat(arquivoEnvio, mensagemDescriptografada);
+        strcat(arquivoEnvio, "\nChave-guia: ");
+        strcat(arquivoEnvio, chaveGuia);
+        strcat(arquivoEnvio, "\n\n\n");
+
+        size_t TamArq = strlen(arquivoEnvio);
+        size_t enviado = fwrite(arquivoEnvio, 1, TamArq, arquivo);
+
+        #if defined(_WIN32) || defined(_WIN64)
+            system("cls");
+        #else 
+            system("clear");
+        #endif
 
         if(enviado != TamArq) {
             printf("Erro no envio da mensagem ao arquivo!\n");
@@ -95,12 +141,40 @@ void criptografarMensagem( char* mens, char guia[4] ) {
     int aux = 0;
     for( x = 0 ; x < tamMSG ; x++ ) {
         const unsigned word = mens[x];
-        unsigned int newWord = newWord = word + interval[aux];;
+        unsigned int newWord = newWord = word + interval[aux];
         if(word == 32) newWord = 35;
+        else if( word == 44) newWord = 43;
 
-        if(newWord > 122) newWord -= 57;
+        if(newWord > 122) newWord -= 58;
 
-        if(newWord > 90 && newWord < 97) newWord += (97 - newWord);
+        if((newWord > 90 && newWord < 97) || (word <= 90 && newWord >= 97)) newWord += 6;
+        mens[x] = newWord;
+        aux++;
+        if(aux > 2) aux = 0;
+    }
+}
+
+void DescriptografarMensagem( char* mens, char guia[4] ) {
+    int x;
+    size_t tamGUIA = strlen(guia);
+    unsigned int interval[tamGUIA];
+    for( x = 0 ; x < tamGUIA; x++ ) {
+        interval[x] = guia[x] - 96;
+    }
+
+    size_t tamMSG = strlen(mens);
+    int aux = 0;
+    for( x = 0 ; x < tamMSG ; x++ ) {
+        const unsigned word = mens[x];
+        unsigned int newWord;
+        if(word == 35) newWord = 32;
+        else if( word == 43) newWord = 44;
+        else newWord = word - interval[aux];
+
+        if(word > 97 && newWord < 97) newWord -= 6;
+
+        if(newWord < 65 && word != 35 && word != 43) newWord += 58;
+
         mens[x] = newWord;
         aux++;
         if(aux > 2) aux = 0;
